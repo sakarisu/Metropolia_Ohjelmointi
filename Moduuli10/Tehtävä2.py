b0c3eb530f389381bfc1b9c2dd3f73d1760f973e
Sakari Surenkin <111981437+sakarisu@users.noreply.github.com>
2022-11-01 21:16:41 +0200

    Päivitys

--- Moduuli10/Tehtävä2.py
+++ Moduuli10/Tehtävä2.py
@@ -1,9 +1,9 @@
 import random
 class Hissi:
-    def __init__(self, ylin, alin, kerros):
+    kerros = 1
+    def __init__(self, ylin, alin):
         self.ylin = ylin
         self.alin = alin
-        self.kerros = kerros
     def kerros_ylos(self):
         self.kerros += 1
         print(f"Hissi on kerroksessa {self.kerros:d}")
@@ -12,15 +12,16 @@ class Hissi:
         print(f"Hissi on kerroksessa {self.kerros:d}")
     def siirry_kerrokseen(self, mihin):
         self.mihin = mihin
-        if self.kerros <= self.mihin:
-            while self.kerros <= self.mihin:
+        print(f"Hissi on kerroksessa {self.kerros:d}")
+        if self.kerros <= mihin:
+            while self.kerros <= mihin:
                 self.kerros_ylos(self)
-                if self.kerros == self.mihin:
+                if self.kerros == mihin:
                     break
-        elif self.kerros >= self.mihin:
-            while self.kerros >= self.mihin:
+        elif self.kerros >= mihin:
+            while self.kerros >= mihin:
                 self.kerros_alas(self)
-                if self.kerros == self.mihin:
+                if self.kerros == mihin:
                     break
 class Talo:
     def __init__(self, ylin, alin, hissit):
@@ -28,11 +29,11 @@ class Talo:
         self.alin = alin
         self.hissit = []
         for i in range(hissit):
-            self.hissit.append(Hissi(5, 1, 1))
+            self.hissit.append(Hissi(5, 1))
     def aja_hissia(self, numero, kohde):
         self.numero = numero
         self.kohde = kohde
         Hissi.siirry_kerrokseen(Hissi, kohde)
 talo = Talo(5, 1, 3)
 setattr(Hissi, 'kerros', 1)
-talo.aja_hissia(random.choice(talo.hissit), int(input('Valitse kerros 1-5 ')))
+talo.aja_hissia(random.choice(talo.hissit), random.randint(1, 5))
