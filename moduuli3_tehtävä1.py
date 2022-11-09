ae5d523c2214710d85befd62a55011fdc446b20f
Sakari Surenkin <111981437+sakarisu@users.noreply.github.com>
2022-11-09 09:36:36 +0200

    Delete moduuli3_tehtävä1.py

--- moduuli3_tehtävä1.py
+++
@@ -1,5 +0,0 @@
-kuha = float(input('Anna kuhan pituus senttimetreinä: '))
-if kuha>=37:
-    print('Voit pitää kuhan.')
-if kuha<=37:
-    print('Kuha on alamittainen, heitä se takaisin veteen. Kuhojen alin sallittu pyyntimitta on 37cm.')
