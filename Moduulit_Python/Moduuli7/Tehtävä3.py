3389ecdbe52d9e95a608f92fb74b576eb66e0630
Sakari Surenkin <111981437+sakarisu@users.noreply.github.com>
2022-11-09 09:16:33 +0200

    Add files via upload

---
+++ Moduulit_Python/Moduuli7/Tehtävä3.py
@@ -0,0 +1,11 @@
+lento = {}
+alku = input('Anna "S" syöttääksesi uuden lentokentän, "H" hakeaksi lentokenttää tai "L" jos haluat lopettaa: ')
+while alku != 'L':
+    if alku == 'S':
+        lento[input('Anna lentokentän ICAO-koodi: ')] = input('Anna lentokentän nimi:')
+        print('Lentokenttä on lisätty tietokantaan.')
+        alku = input('Anna "S" syöttääksesi uuden lentokentän, "H" hakeaksi lentokenttää tai "L" jos haluat lopettaa: ')
+    elif alku == 'H':
+       koodi = input('Anna lentokentän ICAO-koodi: ')
+       if koodi in lento: print(lento[koodi])
+       else: print('Lentokenttää ei ole vielä lisätty tietokantaan.')
