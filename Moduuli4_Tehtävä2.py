668a165f935185cbcd8bc13a3bf54314567304a3
Sakari Surenkin <111981437+sakarisu@users.noreply.github.com>
2022-11-09 09:33:33 +0200

    Delete Moduuli4_Tehtävä2.py

--- Moduuli4_Tehtävä2.py
+++
@@ -1,8 +0,0 @@
-tuuma = int(input('Anna tuuma: '))
-sentti = float(tuuma * 2.54)
-
-while tuuma>=0:
-    print(f"{tuuma:d} tuuma(a) on {sentti:.2f} senttiä.")
-    tuuma = int(input('Anna tuuma: '))
-    if tuuma<=0:
-        break
