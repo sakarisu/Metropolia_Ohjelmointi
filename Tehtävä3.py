2e5567bc2e2a6a5534887bcd838e1b61636e1b32
Sakari Surenkin <111981437+sakarisu@users.noreply.github.com>
2022-11-09 09:35:22 +0200

    Delete Tehtävä3.py

--- Tehtävä3.py
+++
@@ -1,8 +0,0 @@
-kanta_str = input('Anna suorakulmion kanta: ')
-kanta = float(kanta_str)
-korkeus_str = input('Anna suorakulmion korkeus: ')
-korkeus = float(korkeus_str)
-pintaala = kanta * korkeus
-piiri = kanta + korkeus + kanta + korkeus
-print('Suorakulmion pinta-ala on ' + str(pintaala))
-print('Suorakulmion piiri on ' + str(piiri))
