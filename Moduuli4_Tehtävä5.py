9bda996371988dc8a698b2841adfe0f6e9b8b7a8
Sakari Surenkin <111981437+sakarisu@users.noreply.github.com>
2022-11-09 09:34:38 +0200

    Delete Moduuli4_Tehtävä5.py

--- Moduuli4_Tehtävä5.py
+++
@@ -1,7 +0,0 @@
-toistot = 0
-while toistot<5 and (input('Kirjoita käyttäjätunnuksesi: ')!='python' or input('Kirjoita salasanasi: ')!='rules'):
-    toistot+=1
-if toistot<5:
-    print('Tervetuloa.')
-else:
-    print('Pääsy evätty.')
