06eb78de8bef98a02137bfea0554201c0771ba16
Sakari Surenkin <111981437+sakarisu@users.noreply.github.com>
2022-11-09 09:31:13 +0200

    Delete Moduuli6 directory

--- Moduuli6/Moduuli6_Tehtävä3.py
+++
@@ -1,7 +0,0 @@
-def muuntaja(gallona):
-    neste = gallona*3.785411784
-    return neste
-gallona = int(input('Anna bensan määrä gallonissa: '))
-while gallona >= 0:
-    print(str(gallona) + ' gallon(a)a bensaa on litroina ' + str(muuntaja(gallona)) + '.')
-    gallona = int(input('Anna bensan määrä gallonissa: '))
