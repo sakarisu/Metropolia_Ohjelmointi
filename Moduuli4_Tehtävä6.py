b9a0c1ee7c2d0b76ac5d86d1b666165dee6dddee
Sakari Surenkin <111981437+sakarisu@users.noreply.github.com>
2022-11-09 09:34:48 +0200

    Delete Moduuli4_Tehtävä6.py

--- Moduuli4_Tehtävä6.py
+++
@@ -1,9 +0,0 @@
-import math
-import random
-n=0
-N=int(input('Anna testattavat pistet: '))
-for i in range(N):
-    if pow(random.uniform(-1, 1), 2) + pow(random.uniform(-1, 1), 2) < 1:
-        n+=1
-print('Pi:n likiarvo on: ' + str(4*n/N))
-print('Pi:n vakio: ' + str(math.pi))
