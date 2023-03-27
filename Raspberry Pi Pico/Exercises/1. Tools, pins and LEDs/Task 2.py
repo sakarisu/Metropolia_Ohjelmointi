8257ba341ead1aabc2021c8f9be4c0df9a85d790
Sakari Surenkin <111981437+sakarisu@users.noreply.github.com>
2023-03-27 08:22:17 +0300

    Fixed the onboard LED

--- Raspberry Pi Pico/Exercises/1. Tools, pins and LEDs/Task 2.py
+++ Raspberry Pi Pico/Exercises/1. Tools, pins and LEDs/Task 2.py
@@ -1,17 +1,26 @@
 import machine
+import ssd1306
 import utime
 
 leds = [machine.Pin(22, machine.Pin.OUT), machine.Pin(21, machine.Pin.OUT), machine.Pin(20, machine.Pin.OUT)]
-led_pico = machine.Pin(23, machine.Pin.OUT)
+OLED_SCL = 15
+OLED_SDA = 14
+i2c = machine.I2C(1, scl=machine.Pin(OLED_SCL), sda=machine.Pin(OLED_SDA), freq=400000)
+OLED_WIDTH = 128
+OLED_HEIGHT = 64
+oled = ssd1306.SSD1306_I2C(OLED_WIDTH, OLED_HEIGHT, i2c)
+
 
 for led in leds:
     led.value(0)
 
 i = 0
 while True:
-    led_pico.value(1)
+    oled.fill(1)
+    oled.show()
     for j in range(len(leds)):
         leds[j].value((i >> j) & 1)
     i = (i + 1) % 8
     utime.sleep(1)
-    led_pico.value(0)
+    oled.fill(0)
+    oled.show()
