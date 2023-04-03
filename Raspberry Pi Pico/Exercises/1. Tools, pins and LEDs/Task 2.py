a0352b573ecd42ebd729a55cd9e269c290313579
Sakari Surenkin <111981437+sakarisu@users.noreply.github.com>
2023-04-03 14:33:36 +0300

    Updated code

--- Raspberry Pi Pico/Exercises/1. Tools, pins and LEDs/Task 2.py
+++ Raspberry Pi Pico/Exercises/1. Tools, pins and LEDs/Task 2.py
@@ -3,12 +3,8 @@ import ssd1306
 import utime
 
 leds = [machine.Pin(22, machine.Pin.OUT), machine.Pin(21, machine.Pin.OUT), machine.Pin(20, machine.Pin.OUT)]
-OLED_SCL = 15
-OLED_SDA = 14
-i2c = machine.I2C(1, scl=machine.Pin(OLED_SCL), sda=machine.Pin(OLED_SDA), freq=400000)
-OLED_WIDTH = 128
-OLED_HEIGHT = 64
-oled = ssd1306.SSD1306_I2C(OLED_WIDTH, OLED_HEIGHT, i2c)
+i2c = machine.I2C(1, scl=machine.Pin(15), sda=machine.Pin(14), freq=200000)
+oled = ssd1306.SSD1306_I2C(128, 64, i2c)
 
 
 for led in leds:
@@ -17,10 +13,8 @@ for led in leds:
 i = 0
 while True:
     oled.fill(1)
-    oled.show()
     for j in range(len(leds)):
         leds[j].value((i >> j) & 1)
     i = (i + 1) % 8
     utime.sleep(1)
     oled.fill(0)
-    oled.show()
