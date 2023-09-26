398f8ccce5a363fe16dc4c1d092d574e615d0dfd
Sakari Surenkin <111981437+sakarisu@users.noreply.github.com>
2023-09-26 08:08:29 +0300

    Create main.c

---
+++ Exercises_C/Ex13/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+//This program reads integers from a text file and prints the highest and lowest numbers
+int main()
+{       //Main loop
+    char filename[100];
+    printf("Enter a filename: ");       //User needs to enter the full file name (hello.txt, not just hello)
+    scanf("%s", filename);
+    FILE *file = fopen(filename, "r");
+    if (file == NULL)
+    {       //If the file is not found
+        fprintf(stderr, "Error opening file: %s\n", filename);
+        exit(1);
+    }
+    int num, count = 0, min = INT_MAX, max = INT_MIN;
+    while (fscanf(file, "%d", &num) == 1)
+    {       //Lowest and highest numbers are set here
+        count++;
+        if (num < min) min = num;
+        if (num > max) max = num;
+    }
+    printf("Count of numbers: %d\n", count);
+    printf("Lowest number: %d\n", min);
+    printf("Highest number: %d\n", max);
+    fclose(file);
+    return 0;
+}
