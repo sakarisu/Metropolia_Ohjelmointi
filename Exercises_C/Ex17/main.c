c681c519056f524c167a457ff5415cb12db38748
Sakari Surenkin <111981437+sakarisu@users.noreply.github.com>
2023-10-03 07:32:25 +0300

    Update main.c

--- Exercises_C/Ex17/main.c
+++ Exercises_C/Ex17/main.c
@@ -2,24 +2,25 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <stdbool.h>
 
 #define MAX_WORD_LENGTH 32
 
-bool password_generator(char *password, int size, const char *word)
+bool password_generator(char* password, int size, const char* word) 
 {
     int word_length = strlen(word);
     int password_length = word_length * 2 + 1;
-    if(password_length >= size)
+    if (password_length >= size) 
     {
         return false;
     }
     srand(time(NULL));
-    for(int i = 0; i < password_length; i++)
+    for (int i = 0; i < password_length; ++i) 
     {
-        if(i % 2 == 0)
+        if (i % 2 == 0) 
         {
             password[i] = (rand() % (126 - 33 + 1)) + 33;
-        } else
+        } else 
         {
             password[i] = word[i / 2];
         }
@@ -28,26 +29,26 @@ bool password_generator(char *password, int size, const char *word)
     return true;
 }
 
-int main()
+int main() 
 {
     char word[MAX_WORD_LENGTH];
     char password[MAX_WORD_LENGTH * 2 + 1];
-    while(1)
+    bool stop = false;
+    do 
     {
-        printf("Enter a word to place in the password or 'stop' to stop: ");
+        printf("Enter a word or 'stop' to stop: ");
         fgets(word, sizeof(word), stdin);
-        word[strcspn(word, "\n")] = 0;
-        if(strcmp(word, "stop") == 0)
+        word[strcspn(word, "\n")] = '\0';
+        if (strcmp(word, "stop") == 0) 
         {
-            break;
-        }
-        if(password_generator(password, sizeof(password), word))
+            stop = true;
+        } else if (password_generator(password, sizeof(password), word)) 
         {
             printf("Generated password: %s\n", password);
-        } else
+        } else 
         {
-            printf("Error: The generated password does not fit in the string.\n");
+            printf("Error: Password does not fit in the string.\n");
         }
-    }
+    } while (!stop);
     return 0;
 }
